1c780bf944f9b44aabf6752a173eec3b5ef5a1a0
Ares <34551254+AresimaF@users.noreply.github.com>
2023-12-01 16:35:29 -0500

    Initial commit

---
+++ ADC/ADC/Managers/CryptMaster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADC.Managers
+{
+    internal class CryptMaster    {
+        public string standardSaltHash(string toHash)
+        {
+            var salt = BCrypt.Net.BCrypt.GenerateSalt(4);
+            return BCrypt.Net.BCrypt.HashPassword(toHash, salt);
+        }
+
+        public bool verifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
